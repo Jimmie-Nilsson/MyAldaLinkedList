05a301b68f525e19b37190e682e8041ae173e440
move18 <snoozie@hotmail.se>
2024-01-15 20:43:12 +0100

    Initial commit

---
+++ MyALDAList.java
@@ -0,0 +1,53 @@
+import java.util.Iterator;
+
+public class MyALDAList<E> implements ALDAList {
+    @Override
+    public void add(Object element) {
+
+    }
+
+    @Override
+    public void add(int index, Object element) {
+
+    }
+
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object element) {
+        return false;
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(Object element) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(Object element) {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+}
